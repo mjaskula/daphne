2d03beb99a14686b086f2cc2e95d219bbbdf10c4
Matthew Jaskula <mjaskula@gmail.com>
2014-07-24 21:53:34 -0400

    first pass at a text compare of what ages have photos

--- daphne/daphne.py
+++ daphne/daphne.py
@@ -4,26 +4,34 @@ import os
 
 import exifread
 
+def compare(birth_date_a, photo_dir_a, birth_date_b, photo_dir_b):
+	agemap_a = age_map(birth_date_a, photo_dir_a)
+	agemap_b = age_map(birth_date_b, photo_dir_b)
+	for i in range(0, 400):
+		print "{}: {}{}".format(i, '#' * len(agemap_a[i]), '*' * len(agemap_b[i]))
+
+
 def age_map(birth_date, photo_dir):
 	m = defaultdict(list)
 	for file_name in os.listdir(photo_dir):
-		photo_date = get_photo_date(photo_dir, file_name)
+		full_path = photo_dir  + os.sep + file_name
+		photo_date = get_photo_date(full_path)
 		if photo_date:
 			age = (photo_date - birth_date).days
-			m[age].append(file_name)
+			m[age].append(full_path)
 	return m
 
-def get_photo_date(dir, file_name):
-	if file_name.endswith('.MOV'):
+def get_photo_date(full_path):
+	if full_path.endswith('.MOV'):
 		return None
 	
-	tags = get_tags(dir  + os.sep + file_name)
+	tags = get_tags(full_path)
 	datestr = None
 	if 'Image DateTime' in tags:
 		datestr = tags['Image DateTime'].values
 
 	if not datestr:
-		print "skipping {}: it has no date data".format(file_name)
+		print "skipping {}: it has no date data".format(full_path)
 		return 
 
 	return datetime.strptime(datestr, "%Y:%m:%d %H:%M:%S")
@@ -33,9 +41,8 @@ def get_tags(file_name):
 	 	return exifread.process_file(f)
 
 def main():
-	agemap = age_map(datetime(2011, 1, 6), '/Users/matthew/Pictures/evelyn')
-	for i in range(0, 400):
-		print "{}: {}".format(i, '#' * len(agemap[i]))
+	compare(datetime(2011, 1,  6), '/Users/matthew/Documents/devel/daphne/daphne/static/evelyn',
+			datetime(2013, 6, 15), '/Users/matthew/Documents/devel/daphne/daphne/static/penelope')
 
 if __name__== '__main__':
 	main()
