7d2465474d395a47b194c64e7ecd2596bedf1d63
Matthew Jaskula <mjaskula@gmail.com>
2014-11-12 11:08:44 -0500

    evict tabs

--- daphne/daphne.py
+++ daphne/daphne.py
@@ -4,38 +4,38 @@ from itertools import ifilter
 import plistlib
 
 def load_people_from_album_data_file(album_data_file):
-	return build_people_map(load(album_data_file))
+  return build_people_map(load(album_data_file))
 
 def load(album_data_file):
-	print("Loading {}".format(album_data_file))
-	album_data = plistlib.readPlist(album_data_file)
-	print("Loaded {}".format(album_data_file))
-	return album_data
+  print("Loading {}".format(album_data_file))
+  album_data = plistlib.readPlist(album_data_file)
+  print("Loaded {}".format(album_data_file))
+  return album_data
 
 #map of face_key -> {face_data, list of image_data}
 def build_people_map(album_data):
-	people_map = face_data(album_data)
+  people_map = face_data(album_data)
 
-	decorate_with_photos(people_map, album_data)
+  decorate_with_photos(people_map, album_data)
 
-	return people_map
+  return people_map
 
 #map of face_key -> {face_data}
 def face_data(album_data):
-	# return {data['name']:data for id, data in album_data['List of Faces'].iteritems()}
-	faces = album_data['List of Faces']
-	for face in faces.values():
-		print "{} - {} - {}".format(face['key'], face['PhotoCount'], face['name'])
-		face['images'] = []
-	return faces
+  # return {data['name']:data for id, data in album_data['List of Faces'].iteritems()}
+  faces = album_data['List of Faces']
+  for face in faces.values():
+    print "{} - {} - {}".format(face['key'], face['PhotoCount'], face['name'])
+    face['images'] = []
+  return faces
 
 def decorate_with_photos(people_map, album_data):
-	for image in images_with_faces(album_data):
-		for face in image['Faces']:
-			people_map[face['face key']]['images'].append(image)
+  for image in images_with_faces(album_data):
+    for face in image['Faces']:
+      people_map[face['face key']]['images'].append(image)
 
 def images_with_faces(album_data):
-	return ifilter(lambda img: 'Faces' in img, album_data['Master Image List'].values())
+  return ifilter(lambda img: 'Faces' in img, album_data['Master Image List'].values())
 
 
 APPLE_EPOCH = datetime(2001, 1, 1)
--- daphne/daphne_dir.py
+++ daphne/daphne_dir.py
@@ -5,45 +5,45 @@ import os
 import exifread
 
 def compare(birth_date_a, photo_dir_a, birth_date_b, photo_dir_b):
-	agemap_a = age_map(birth_date_a, photo_dir_a)
-	agemap_b = age_map(birth_date_b, photo_dir_b)
-	for i in range(0, 400):
-		print "{}: {}{}".format(i, '#' * len(agemap_a[i]), '*' * len(agemap_b[i]))
+  agemap_a = age_map(birth_date_a, photo_dir_a)
+  agemap_b = age_map(birth_date_b, photo_dir_b)
+  for i in range(0, 400):
+    print "{}: {}{}".format(i, '#' * len(agemap_a[i]), '*' * len(agemap_b[i]))
 
 # map of age -> [photo paths]
 def age_map(birth_date, photo_dir):
-	m = defaultdict(list)
-	for file_name in os.listdir(photo_dir):
-		full_path = photo_dir  + os.sep + file_name
-		photo_date = get_photo_date(full_path)
-		if photo_date:
-			age = (photo_date - birth_date).days
-			m[age].append(full_path)
-	return m
+  m = defaultdict(list)
+  for file_name in os.listdir(photo_dir):
+    full_path = photo_dir  + os.sep + file_name
+    photo_date = get_photo_date(full_path)
+    if photo_date:
+      age = (photo_date - birth_date).days
+      m[age].append(full_path)
+  return m
 
 def get_photo_date(full_path):
-	if full_path.endswith('.MOV'):
-		return None
-	
-	tags = get_tags(full_path)
-	datestr = None
-	if 'Image DateTime' in tags:
-		datestr = tags['Image DateTime'].values
+  if full_path.endswith('.MOV'):
+    return None
+  
+  tags = get_tags(full_path)
+  datestr = None
+  if 'Image DateTime' in tags:
+    datestr = tags['Image DateTime'].values
 
-	if not datestr:
-		print "skipping {}: it has no date data".format(full_path)
+  if not datestr:
+    print "skipping {}: it has no date data".format(full_path)
     #TODO check for other date info/tags
-		return 
+    return 
 
-	return datetime.strptime(datestr, "%Y:%m:%d %H:%M:%S")
+  return datetime.strptime(datestr, "%Y:%m:%d %H:%M:%S")
 
 def get_tags(file_name):
-	with open(file_name, 'rb') as f:
-	 	return exifread.process_file(f)
+  with open(file_name, 'rb') as f:
+     return exifread.process_file(f)
 
 def main():
-	compare(datetime(2011, 1,  6), '/Users/matthew/Documents/devel/daphne/daphne/static/evelyn',
-			datetime(2013, 6, 15), '/Users/matthew/Documents/devel/daphne/daphne/static/penelope')
+  compare(datetime(2011, 1,  6), '/Users/matthew/Documents/devel/daphne/daphne/static/evelyn',
+      datetime(2013, 6, 15), '/Users/matthew/Documents/devel/daphne/daphne/static/penelope')
 
 if __name__== '__main__':
-	main()
+  main()
