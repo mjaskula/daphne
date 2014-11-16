6755a4b1c550e414ecca3e320aa99b1620742c69
Matthew Jaskula <mjaskula@gmail.com>
2014-11-15 22:41:36 -0500

    angular service for the backend, added people flask endpoint

--- daphne/server.py
+++ daphne/server.py
@@ -1,12 +1,22 @@
-from flask import Flask, make_response
 from datetime import datetime
 
+from flask import Flask, make_response, jsonify, Response 
+from bson import json_util
+
+import mongo as db
 
 app = Flask("daphne")
 
 @app.route('/')
 def index():
   return make_response(open('templates/index.html').read())
+
+@app.route('/people.json')
+def people():
+  return Response(
+    json_util.dumps(db.people()),
+    mimetype='application/json'
+  )
 
 #Old endpoints
 
--- static/js/daphne.js
+++ static/js/daphne.js
@@ -1,13 +1,18 @@
-if(typeof angular == 'undefined') {
-  console.log("no angular")
-} else {
-  console.log("angular")
-}
 
-var daphne = angular.module('daphne',[]);
+var daphneServices = angular.module('daphneServices', ['ngResource']);
+
+daphneServices.factory('People', ['$resource',
+  function($resource){
+    return $resource('people.json', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }]);
+
+
+var daphne = angular.module('daphne', ['daphneServices']);
 
 console.log("loaded")
 
-daphne.controller('DaphneController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
+daphne.controller('DaphneController', ['$scope', 'People', function($scope, People) {
+  $scope.people = People.query();
 }]);
