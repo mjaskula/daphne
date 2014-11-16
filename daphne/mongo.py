706e4d195b394e9f696f35afd1217ebb76942bd7
Matthew Jaskula <mjaskula@gmail.com>
2014-11-15 22:39:54 -0500

    [mongo] removed the collection vars, added people() accesor

--- daphne/mongo.py
+++ daphne/mongo.py
@@ -3,13 +3,11 @@ from pymongo import MongoClient, ASCENDING
 mongo_uri = 'mongodb://localhost:27017/'
 
 db = MongoClient(mongo_uri).daphne
-images = db.images
-images.create_index([('key', ASCENDING), 
-                    ('ImagePath', ASCENDING)], 
-                    unique=True)
-images.create_index([('age', ASCENDING)])
-people = db.people
-people.create_index('key', unique=True)
+db.images.create_index([('key', ASCENDING), 
+                        ('ImagePath', ASCENDING)], 
+                       unique=True)
+db.images.create_index([('age', ASCENDING)])
+db.people.create_index('key', unique=True)
 
 
 def write(person):
@@ -19,14 +17,17 @@ def write(person):
   _write_person(person)
 
 def _write_image(image):
-  images.update({'key': image['key'], 'ImagePath': image['ImagePath']}, 
-                 image, 
-                 upsert=True)
+  db.images.update({'key': image['key'], 'ImagePath': image['ImagePath']}, 
+                   image, 
+                   upsert=True)
 
 def _write_person(person):
-  people.update({'key': person['key']}, person, upsert=True)
+  db.people.update({'key': person['key']}, person, upsert=True)
+
+def people():
+  return [p for p in db.people.find()]
 
 def get_images(key, days_min, days_max):
-  return images.find({'key': key, 
+  return db.images.find({'key': key, 
                       'age': {'$gt': days_min, 
                               '$lt': days_max}})
